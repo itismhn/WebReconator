282092d4a4eeab995c73bae6002f7547b4ccf1e3
Mohammad Hossein Nouri <mmdnoori13800@gmail.com>
2025-04-27 13:28:52 +0330

    complete headersafe part

--- webreconator.py
+++ webreconator.py
@@ -1,3 +1,5 @@
+from modules.headersafe import check_security_headers
+
 def show_menu():
     print("\n====== WebReconator ======")
     print("1. HeaderSafe (Check Security Headers)")
@@ -11,8 +13,8 @@ def main():
         choice = input("Enter your choice: ").strip()
 
         if choice == "1":
-            #check_security_headers(url)
-            print("1")
+            check_security_headers(url)
+            break
         elif choice == "0":
             print("Goodbye!")
             break

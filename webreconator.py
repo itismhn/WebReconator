0f8e3bccbafb2821cb5507d970b7cd30106e0a71
Mohammad Hossein Nouri <mmdnoori13800@gmail.com>
2025-04-27 13:24:09 +0330

    def menu func

--- webreconator.py
+++ webreconator.py
@@ -1,5 +1,10 @@
+def show_menu():
+    print("\n====== WebReconator ======")
+    print("1. HeaderSafe (Check Security Headers)")
+    print("0. Exit!")
+    print("==========================")
+
 def main():
     url=input("Enter the URL (include http/https): ").strip()
-
 if __name__ == "__main__":
     main()

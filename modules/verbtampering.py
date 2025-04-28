2bed0ff73293c80ac193d16308d47089c610a8c1
Mohammad Hossein Nouri <mmdnoori13800@gmail.com>
2025-04-28 09:11:48 +0330

    some err fixes

--- modules/verbtampering.py
+++ modules/verbtampering.py
@@ -3,7 +3,9 @@ http_methods = ['GET', 'POST', 'PUT', 'DELETE', 'HEAD', 'OPTIONS', 'PATCH', 'TRA
 def http_requests(URL):
     for method in http_methods:
         try:
-            response = requests.request(method, URL)
-            print(f"[~] {method} {response.status_code} {response.reason}")
+            response = requests.request(method, URL, allow_redirects=False)
+            print(f"[~] {method} => Status: {response.status_code} - {response.reason}")
+            if response.status_code == 405:
+                print(f"    [!] {method} is NOT allowed")
         except requests.exceptions.RequestException as e:
             print(f"[!] {method} request failed: {e}")

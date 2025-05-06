01a58d5408ecf63e7b2b3c7bf41ae05f17f13821
Mohammad Hossein Nouri <mmdnoori13800@gmail.com>
2025-05-06 12:34:18 +0330

    adding whois module

---
+++ modules/who_is.py
@@ -0,0 +1,23 @@
+import whois
+
+def get_whois_info(domain):
+    try:
+        w = whois.whois(domain)
+        return w
+    except Exception as e:
+        return w
+    except Exception as e:
+        print(f"[ERROR] Failed to fetch WHOIS for {domain}: {e}")
+        return None
+
+def display_whois_info(whois_data):
+    if not whois_data:
+        print("No data to display.")
+        return
+
+    print("\nWHOIS Information:")
+    print(f"Domain Name: {whois_data.domain_name}")
+    print(f"Registrar: {whois_data.registrar}")
+    print(f"Creation Date: {whois_data.creation_date}")
+    print(f"Expiration Date: {whois_data.expiration_date}")
+    print(f"Name Servers: {', '.join(whois_data.name_servers)}")
--- webreconator.py
+++ webreconator.py
@@ -1,11 +1,13 @@
 from modules.headersafe import check_security_headers
 from modules.verbtampering import http_requests
 from modules.sencheck import check_important_files
+from modules.who_is import get_whois_info, display_whois_info
 def show_menu():
     print("\n====== WebReconator ======")
     print("1. HeaderSafe (Check Security Headers)")
     print("2. Verb Tampering (Check HTTP Methods)")
     print("3. Sensitive Directory Check")
+    print("4. Whois the domain")
 
     print("0. Exit!")
     print("==========================")
@@ -23,6 +25,10 @@ def main():
             break
         elif choice == "3":
             check_important_files(url)
+            break
+        elif choice == "4":
+            whois_data = get_whois_info(url)
+            display_whois_info(whois_data)
             break
         elif choice == "0":
             print("Goodbye!")

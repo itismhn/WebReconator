f12d4e94a0cc524d07b1ac560a8a9dcc07618fe8
Mohammad Hossein Nouri <mmdnoori13800@gmail.com>
2025-05-12 14:35:38 +0330

    add ssl_inspector option

--- webreconator.py
+++ webreconator.py
@@ -2,13 +2,15 @@ from modules.headersafe import check_security_headers
 from modules.verbtampering import http_requests
 from modules.sencheck import check_important_files
 from modules.who_is import get_whois_info, display_whois_info
+from modules.ssl_inspector import ssl_inspect
+
 def show_menu():
     print("\n====== WebReconator ======")
     print("1. HeaderSafe (Check Security Headers)")
     print("2. Verb Tampering (Check HTTP Methods)")
     print("3. Sensitive Directory Check")
     print("4. Whois the domain")
-
+    print("5. SSL Insoector (Get All Supported Cipher Suites)")
     print("0. Exit!")
     print("==========================")
 
@@ -29,6 +31,9 @@ def main():
         elif choice == "4":
             whois_data = get_whois_info(url)
             display_whois_info(whois_data)
+            break
+        elif choice == "5":
+            info = ssl_inspect(url, 443)
             break
         elif choice == "0":
             print("Goodbye!")

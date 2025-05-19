a93598204a6c935e1b35319983c6f6d50bd62b46
Mohammad Hossein Nouri <mmdnoori13800@gmail.com>
2025-05-19 10:45:31 +0330

    string changes

--- modules/verbtampering.py
+++ modules/verbtampering.py
@@ -1,10 +1,11 @@
 import requests
 http_methods = ['GET', 'POST', 'PUT', 'DELETE', 'HEAD', 'OPTIONS', 'PATCH', 'TRACE', 'CONNECT']
 def http_requests(URL):
+    print("\n[+] Checking for Supported HTTP Methods:")
     for method in http_methods:
         try:
             response = requests.request(method, URL, allow_redirects=False)
-            print(f"[~] {method} => Status: {response.status_code} - {response.reason}")
+            print(f"[-] {method} => Status: {response.status_code} - {response.reason}")
             if response.status_code == 405:
                 print(f"    [!] {method} is NOT allowed")
         except requests.exceptions.RequestException as e:
